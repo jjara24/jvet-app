855a6200be6b33b6623643af80847dcbbb8fa185
jjara24 <lucasjara24@gmail.com>
2024-04-12 15:26:31 -0300

    create pages and header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,18 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import About from './pages/About';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-      </Routes>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/sign-in' element={<SignIn/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
     </BrowserRouter>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <h1>SignUp</h1>
   )
 }
